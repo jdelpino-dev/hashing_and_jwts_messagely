452208dfbd1e97cac4fc0478785f5f13568a0cb4
José Delpino <delpinoivivas@gmail.com>
2024-05-22 11:41:07 -0500

    feat: Added login and register logic to the auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,6 +3,7 @@
 import jwt from "jsonwebtoken";
 import { SECRET_KEY } from "../config.js";
 import ExpressError from "../expressError.js";
+import User from "../models/user.js";
 
 /** Middleware: Authenticate user. */
 // I updated this function to use current best practices for token handling.
@@ -37,8 +38,6 @@ function ensureLoggedIn(req, res, next) {
 }
 
 /** Middleware: Requires correct username. */
-
-/** Middleware: Requires correct username. */
 function ensureCorrectUser(req, res, next) {
   if (!req.user || req.user.username !== req.params.username) {
     return next(new ExpressError("Unauthorized", 401));
@@ -47,4 +46,53 @@ function ensureCorrectUser(req, res, next) {
   }
 }
 
-export { authenticateJWT, ensureCorrectUser, ensureLoggedIn };
+async function loginUser(req, res, next) {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      throw new ExpressError("Username and password required", 400);
+    }
+    if (User.authenticate(username, password)) {
+      await User.updateLoginTimestamp(username);
+      const token = jwt.sign({ username }, SECRET_KEY);
+      return res.json({ message: `Logged in!`, token });
+    }
+    throw new ExpressError("Invalid username/password", 400);
+  } catch (e) {
+    return next(e);
+  }
+}
+
+async function registerUser(req, res, next) {
+  try {
+    const { username, password, first_name, last_name, phone } = req.body;
+    if (!username || !password || !first_name || !last_name || !phone) {
+      throw new ExpressError("All fields required", 400);
+    }
+    await User.register({
+      username,
+      password,
+      first_name,
+      last_name,
+      phone,
+    });
+    const token = jwt.sign({ username }, SECRET_KEY);
+    return res.json({ message: `Welcome ${username}!`, token });
+  } catch (err) {
+    if (err.code === "23505") {
+      // 23505 is the Postgres unique constraint error code
+      return next(
+        new ExpressError("Username taken. Please pick another!", 400)
+      );
+    }
+    return next(new ExpressError(err, 400));
+  }
+}
+
+export {
+  authenticateJWT,
+  ensureCorrectUser,
+  ensureLoggedIn,
+  loginUser,
+  registerUser,
+};
