a62b9a14a030d72eb769ffadcba997cd9e83d916
José Delpino <delpinoivivas@gmail.com>
2024-05-22 13:34:32 -0500

    refactor: implement all user routes

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,41 @@
+import { default as Router } from "express";
+import User from "../models/user.js";
+import {
+  authenticateJWT,
+  ensureCorrectUser,
+  ensureLoggedIn,
+} from "./middleware/auth.js";
+
+const router = new Router();
+
 /** GET / - get list of users.
  *
  * => {users: [{username, first_name, last_name, phone}, ...]}
  *
  **/
+router.get("/", authenticateJWT, ensureLoggedIn, (req, res) => {
+  res.json({ users: User.all() });
+});
 
 /** GET /:username - get detail of users.
  *
  * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
  *
  **/
+router.get(
+  "/:username",
+  authenticateJWT,
+  ensureLoggedIn,
+  ensureCorrectUser,
+  (req, res) => {
+    const user = User.get(req.params.username);
+    if (user) {
+      res.json({ user: user });
+    } else {
+      res.status(404).json({ error: "User not found" });
+    }
+  }
+);
 
 /** GET /:username/to - get messages to user
  *
@@ -19,6 +46,16 @@
  *                 from_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
+router.get(
+  "/:username/to",
+  authenticateJWT,
+  ensureLoggedIn,
+  ensureCorrectUser,
+  (req, res) => {
+    const messages = User.messagesTo(req.params.username);
+    res.json({ messages: messages });
+  }
+);
 
 /** GET /:username/from - get messages from user
  *
@@ -29,3 +66,15 @@
  *                 to_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
+router.get(
+  ":username/from",
+  authenticateJWT,
+  ensureLoggedIn,
+  ensureCorrectUser,
+  (req, res) => {
+    const messages = User.messagesFrom(req.params.username);
+    res.json({ messages: messages });
+  }
+);
+
+export default router;
