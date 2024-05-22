e72c863fc00b82c97e34db55e1fdcbb7303812dd
José Delpino <delpinoivivas@gmail.com>
2024-05-22 09:50:00 -0500

    Refactor tests to use ES modules and update imports

--- __tests__/messages.test.js
+++ __tests__/messages.test.js
@@ -1,38 +1,38 @@
-const db = require("../db");
-const User = require("../models/user");
-const Message = require("../models/message");
-
+import db from "../db.js";
+import Message from "../models/message.js";
+import User from "../models/user.js";
 
 describe("Test Message class", function () {
-
+  let u1, u2;
+  let m1, m2;
   beforeEach(async function () {
     await db.query("DELETE FROM messages");
     await db.query("DELETE FROM users");
     await db.query("ALTER SEQUENCE messages_id_seq RESTART WITH 1");
 
-    let u1 = await User.register({
+    u1 = await User.register({
       username: "test1",
       password: "password",
       first_name: "Test1",
       last_name: "Testy1",
       phone: "+14155550000",
     });
-    let u2 = await User.register({
+    u2 = await User.register({
       username: "test2",
       password: "password",
       first_name: "Test2",
       last_name: "Testy2",
       phone: "+14155552222",
     });
-    let m1 = await Message.create({
+    m1 = await Message.create({
       from_username: "test1",
       to_username: "test2",
-      body: "u1-to-u2"
+      body: "u1-to-u2",
     });
-    let m2 = await Message.create({
+    m2 = await Message.create({
       from_username: "test2",
       to_username: "test1",
-      body: "u2-to-u1"
+      body: "u2-to-u1",
     });
   });
 
@@ -40,7 +40,7 @@ describe("Test Message class", function () {
     let m = await Message.create({
       from_username: "test1",
       to_username: "test2",
-      body: "new"
+      body: "new",
     });
 
     expect(m).toEqual({
@@ -53,17 +53,9 @@ describe("Test Message class", function () {
   });
 
   test("can mark read", async function () {
-    let m = await Message.create({
-      from_username: "test1",
-      to_username: "test2",
-      body: "new"
-    });
-    expect(m.read_at).toBe(undefined);
-
-    Message.markRead(m.id);
-    const result = await db.query("SELECT read_at from messages where id=$1",
-        [m.id]);
-    expect(result.rows[0].read_at).toEqual(expect.any(Date));
+    Message.markRead(m1.id);
+    m1 = await Message.get(m1.id);
+    expect(m1.read_at).toEqual(expect.any(Date));
   });
 
   test("can get", async function () {
@@ -89,6 +81,6 @@ describe("Test Message class", function () {
   });
 });
 
-afterAll(async function() {
+afterAll(async function () {
   await db.end();
 });
